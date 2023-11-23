d1d251eff1afd33bb5dc8ffa56818501a369393c
Rafael <rafaelpmeirelles@gmail.com>
2023-11-23 16:00:41 -0300

    Criando app de accounts

--- SunBlog/SunBlog/settings.py
+++ SunBlog/SunBlog/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog',
     'api',
+    'accounts',
     'rest_framework',
     'corsheaders'
 ]
@@ -134,11 +135,11 @@ CORS_ORIGIN_WHITELIST = [
     'http://localhost:8080',
 ]
 
-# REST_FRAMEWORK = {
-# 'DEFAULT_SCHEMA_CLASS':
-# 'rest_framework.schemas.coreapi.AutoSchema',
-# 'DEFAULT_AUTHENTICATION_CLASSES': [
-# 'rest_framework.authentication.TokenAuthentication'
-# ,
-# ],
-# }
+REST_FRAMEWORK = {
+'DEFAULT_SCHEMA_CLASS':
+'rest_framework.schemas.coreapi.AutoSchema',
+'DEFAULT_AUTHENTICATION_CLASSES': [
+'rest_framework.authentication.TokenAuthentication'
+,
+],
+}
