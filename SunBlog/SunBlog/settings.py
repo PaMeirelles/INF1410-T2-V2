397666d76a6f68132e8f5a9298b71b4984d5098a
Rafael <rafaelpmeirelles@gmail.com>
2023-11-23 15:05:12 -0300

    Incluindo rest_framework

--- SunBlog/SunBlog/settings.py
+++ SunBlog/SunBlog/settings.py
@@ -133,3 +133,12 @@ CORS_ORIGIN_WHITELIST = [
     'http://127.0.0.1:8080',
     'http://localhost:8080',
 ]
+
+REST_FRAMEWORK = {
+'DEFAULT_SCHEMA_CLASS':
+'rest_framework.schemas.coreapi.AutoSchema',
+'DEFAULT_AUTHENTICATION_CLASSES': [
+'rest_framework.authentication.TokenAuthentication'
+,
+],
+}
