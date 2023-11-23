592a9770cb9315aacbed8321a3ab0eafedce49be
Rafael <rafaelpmeirelles@gmail.com>
2023-11-22 21:08:05 -0300

    Tela de insert existe (não testada)

--- SunBlog/blog/urls.py
+++ SunBlog/blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import blog_posts
+from .views import blog_posts, insert_post
 
+app_name = 'blog'
 
 urlpatterns = [
-    path('posts/', blog_posts, name='blog_posts')
-
+    path('posts/', blog_posts, name='blog_posts'),
+    path('insert/', insert_post, name='insert_post')
 ]
--- SunBlog/blog/views.py
+++ SunBlog/blog/views.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 import requests
 from django.shortcuts import render
 from rest_framework import status
+from .forms import PostForm
 
 
 def home(request):
@@ -16,3 +17,32 @@ def blog_posts(request):
     blog_posts = response.json()
 
     return render(request, 'blog/blog_posts.html', {'blog_posts': blog_posts})
+
+def insert_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post_data = {
+                'titulo': form.cleaned_data['titulo'],
+                'slug': form.cleaned_data['slug'],
+                'autor': form.cleaned_data['autor'].id,
+                'corpo': form.cleaned_data['corpo'],
+                'dt_publicado': form.cleaned_data['dt_publicado'].isoformat(),
+                'status': form.cleaned_data['status'],
+            }
+
+            api_url = 'http://127.0.0.1:8000/api/umpost/'
+            response = requests.post(api_url, json=post_data)
+
+            # Check if the request was successful
+            if response.status_code == 201:  # Assuming 201 means created
+                # Redirect or handle success as needed
+                return redirect('success_url')
+            else:
+                # Handle the error, e.g., by displaying an error message
+                error_message = f"Failed to create post. API responded with {response.status_code} status."
+                return render(request, 'blog/error.html', {'error_message': error_message})
+    else:
+        form = PostForm()
+
+    return render(request, 'blog/new_blog.html', {'form': form})
