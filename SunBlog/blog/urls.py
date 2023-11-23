97f2ab6c3d7fe1f10842262905daa24267cc99e9
Sol <solmseda@gmail.com>
2023-11-23 15:59:31 -0300

    Correção de um erro que ocorreu em urls.py após o merge

--- SunBlog/blog/urls.py
+++ SunBlog/blog/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('api/blog/posts/', api_blog_posts, name='api_blog_posts'),
     path('posts/', blog_posts, name='blog_posts'),
     path('insert/', insert_post, name='insert_post'),
-    path('edit_post/<int:post_id>/', edit_post, name='edit_post')
+    path('edit_post/<int:post_id>/', edit_post, name='edit_post'),
     path('post/<int:post_id>/', post_detail, name='post_detail'),
 ]
