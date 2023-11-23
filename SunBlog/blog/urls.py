86e6eb1a2d93b3a2d9e3b147d374057920e212a8
Sol <solmseda@gmail.com>
2023-11-23 15:35:43 -0300

    Não utilizar mais o endereço url da api explicitamente

--- SunBlog/blog/urls.py
+++ SunBlog/blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import api_blog_posts, blog_posts, insert_post, edit_post
+from .views import api_blog_posts, blog_posts, insert_post, edit_post, post_detail
 
 app_name = 'blog'
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path('posts/', blog_posts, name='blog_posts'),
     path('insert/', insert_post, name='insert_post'),
     path('edit_post/<int:post_id>/', edit_post, name='edit_post'),
+    path('post/<int:post_id>/', post_detail, name='post_detail'),
 ]
--- SunBlog/frontend/public/javascript/script.js
+++ SunBlog/frontend/public/javascript/script.js
@@ -16,8 +16,11 @@ function exibeLista() {
             for (const post of posts) {
                 const postElement = document.createElement('div');
                 postElement.className = 'post';
+                const postLink = document.createElement('a');
+                postLink.href = backendAddress + 'blog/post/' + post.id;
+                postLink.textContent = post.titulo;
                 postElement.innerHTML = `
-                        <h2>${post.titulo}</h2>
+                        <h1>${postLink.outerHTML}</h1>
                         <p>${post.corpo}</p>
                         <p>Publicado em ${post.dt_publicado} por ${post.autor}</p>
                     `;
@@ -27,8 +30,31 @@ function exibeLista() {
         else {
             console.error('Element with id "post-list" not found');
         }
+    });
+}
+function exibeDetalhes(postId) {
+    fetch(backendAddress + "blog/api/blog/post/" + postId)
+        .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+    })
+        .then(post => {
+        const postElement = document.getElementById('post-detail');
+        if (postElement) {
+            postElement.innerHTML = `
+                    <h1>${post.titulo}</h1>
+                    <p>${post.corpo}</p>
+                    <p>Publicado em ${post.dt_publicado} por ${post.autor}</p>
+                    <a href="/blog/posts/">Voltar para a lista de posts</a>
+                `;
+        }
+        else {
+            console.error('Element with id "post-detail" not found');
+        }
     })
         .catch(error => {
-        console.error('An error occurred while fetching the posts:', error);
+        console.error('An error occurred while fetching the post:', error);
     });
 }
