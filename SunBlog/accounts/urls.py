9e3dc65f35f0e8e959eb800e30c703fdfe7fba4b
Sol <solmseda@gmail.com>
2023-11-23 23:57:25 -0300

    adicionei o cabeçalho na pagina de listar os posts e coloquei o css

--- SunBlog/accounts/urls.py
+++ SunBlog/accounts/urls.py
@@ -4,6 +4,6 @@ from .views import login_page
 app_name = 'accounts'
 
 urlpatterns = [
-    path("login/", login_page),
+    path("login/", login_page, name='login'),
     path('token-auth/', views.CustomAuthToken.as_view(), name='token-auth'),
 ]
